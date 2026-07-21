27f9e4a94587632d036c54690b97f6dca835d942
root <root@DMED7596.win.ntnu.no>
2026-07-21 20:08:04 +0200

    reduce RAM in WSL, remove docker

--- run.sh
+++ run.sh
@@ -15,4 +15,13 @@ java --version
 #nextflow  self-update
 /cluster/home/ash022/scripts/nextflow -v
 #nextflow version 26.04.6.12646
-nextflow run https://github.com/nf-core/sarek --revision 3.9.0 --input ./PGTK/samples.csv --outdir resultsTKvep -profile docker -c <(echo "process { withName: 'NFCORE_SAREK:SAREK:FASTQ_PREPROCESS_GATK:FASTQ_ALIGN:BWAMEM1_MEM' { cpus = 12; ext.args = { '-t 12' }; ext.args2 = { '--threads 12' } } }")  --tools haplotypecaller,vep --max_memory '64.GB' --max_cpus 12 -resume
+sudo apt purge apptainer apptainer-suid
+sudo apt autoremove
+# Add the key
+curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/apptainer.gpg > /dev/null
+# Add the repository to your sources list
+echo "deb [signed-by=/usr/share/keyrings/apptainer.gpg] https://packages.microsoft.com/debian/$(lsb_release -cs)/prod $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/apptainer.list
+# Update and install
+sudo apt update
+sudo apt install -y apptainer
+nextflow run https://github.com/nf-core/sarek --revision 3.9.0 --input ./PGTK/samples.csv --outdir resultsTKvep -profile singularity -c <(echo "process { withName: 'NFCORE_SAREK:SAREK:FASTQ_PREPROCESS_GATK:FASTQ_ALIGN:BWAMEM1_MEM' { cpus = 12; ext.args = { '-t 12' }; ext.args2 = { '--threads 12' } } }")  --tools haplotypecaller,vep --max_memory '64.GB' --max_cpus 12 -resume
